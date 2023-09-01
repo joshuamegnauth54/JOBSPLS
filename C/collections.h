7e4beb496c4fffab13eabe41d69275aca684f943
Josh Megnauth <joshua.megnauth54@gmail.com>
2023-09-01 07:04:37 -0400

    Add API for C vector.

---
+++ C/collections.h
@@ -0,0 +1,8 @@
+// Collections implemented in C
+
+#ifndef JOSH_COLLECTIONS_H
+#define JOSH_COLLECTIONS_H
+
+#include "collections/vector.h"
+
+#endif
---
+++ C/collections/vector.h
@@ -0,0 +1,56 @@
+// Vector
+#ifndef JOSH_COLLECTIONS_VECTOR_H
+#define JOSH_COLLECTIONS_VECTOR_H
+
+#include <stddef.h>
+#include <sys/types.h>
+
+struct Vector {
+    // Array of size `len` to any type, T
+    u_int8_t const* data;
+    // Capacity of `data` in terms of how many T may be stored
+    size_t capacity;
+    // Number of type T stored in `data` (a.k.a. length)
+    size_t pos;
+    // Size of type T
+    size_t const data_size;
+};
+
+enum VectorErrors {
+    // Item not found
+    NOITEM
+};
+
+// Create a new Vector that stores data of size `data_size`.
+//
+// # Arguments
+// * data_size: Size of type T in bytes.
+//   For example:
+//               1. int32 values are 4 bytes
+//               2. Pointers to anything are the size of the pointer
+//               3. A struct is the size of the struct value
+Vector* vec_new(size_t const data_size);
+
+Vector* vec_with_capacity(size_t const data_size, size_t const cap);
+
+void vec_free(Vector* const vec);
+
+// Delete a Vector and call a function on each value for clean up.
+//
+// NOTE: dfree should be cognizant of type T as each value is passed as is.
+// T may be the value of pointers or structs by value.
+// The caller should know the type.
+void vec_free_with(Vector* const vec, void dfree(void*));
+
+// Map T => U
+Vector* vec_map(const Vector* const vec, const size_t data_size, void* map(void*));
+
+void vec_push(Vector* const vec, void const* const value);
+
+void* vec_pop(Vector* const vec);
+
+void* vec_get(Vector const* const vec, size_t const index);
+
+void* vec_remove(Vector* const vec, size_t const index);
+
+#endif

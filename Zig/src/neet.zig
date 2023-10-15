3cce7be21d27e078d45995992bcee7f9f10a5c47
Josh Megnauth <joshua.megnauth54@gmail.com>
2023-10-15 19:28:50 -0400

    Update k_largest to use comparison funcs (Zig)

--- Zig/src/neet.zig
+++ Zig/src/neet.zig
@@ -11,6 +11,7 @@ pub const substr = @import("neet/substring.zig").substr;
 pub const last_word_len = @import("neet/last_word_len.zig").last_word_len;
 pub const ResTwoSum = @import("neet/two_sum.zig").ResTwoSum;
 pub const two_sum = @import("neet/two_sum.zig").two_sum;
+pub const two_sum_sorted = @import("neet/two_sum_sorted.zig").two_sum_sorted;
 pub const longest_prefix = @import("neet/longest_prefix.zig").longest_prefix;
 pub const group_anagrams = @import("neet/group_anagrams.zig").group_anagrams;
 pub const k_largest = @import("neet/k_largest.zig").k_largest;
@@ -18,3 +19,5 @@ pub const valid_sudoku = @import("neet/valid_sudoku.zig").valid_sudoku;
 // pub const encode_str = @import("neet/")
 // pub const logger_rate_limit =
 // pub const ranked_voting
+// pub const consec_sequence
+pub const valid_palindrome = @import("neet/valid_palindrome.zig").valid_palindrome;
--- Zig/src/neet/k_largest.zig
+++ Zig/src/neet/k_largest.zig
@@ -2,13 +2,34 @@ const std = @import("std");
 const Allocator = std.mem.Allocator;
 const PriorityQueue = std.PriorityQueue;
 const Order = std.math.Order;
+const expectEqual = std.testing.expectEqual;
 
 pub fn k_largest(comptime T: type, allocator: Allocator, haystack: []const T, k: usize, comptime compfunc: ?fn (void, T, T) Order) !T {
+    const compfunc_def = comptime switch (@typeInfo(T)) {
+        .Int, .ComptimeInt, .Float, .ComptimeFloat => opcomp: {
+            if (compfunc) |f| {
+                break :opcomp f;
+            } else {
+                break :opcomp std.sort.desc(T);
+            }
+        },
+        else => needcomp: {
+            if (compfunc) |f| {
+                break :needcomp f;
+            } else {
+                @compileError("Types other than ints or floats need a compfunc\n");
+            }
+        },
+    };
+
+    // Can't return the kth largest if k > len
     if (k >= haystack.len) {
         return null;
     }
 
-    var queue = PriorityQueue(T, {}, compfunc).init(allocator, void);
+    // Logic here is to shunt all of the items into a priority queue which is ordered
+    // Next, just pop the largest k values till I reach the required item
+    var queue = PriorityQueue(T, {}, compfunc_def).init(allocator, void);
     defer queue.deinit();
     try queue.ensureTotalCapacity(haystack.len);
     try queue.addSlice(haystack);
@@ -21,5 +42,22 @@ pub fn k_largest(comptime T: type, allocator: Allocator, haystack: []const T, k:
         }
     }
 
+    // Pop the kth if it exists
     return queue.removeOrNull();
+}
+
+test "kth value exists" {
+    const haystack = [_]u8{ 28, 42, 24, 14, 64, 7 };
+    const res = k_largest(u8, std.testing.allocator, &haystack, 3, null);
+    if (res) |target| {
+        try expectEqual(target, 28);
+    } else {
+        @panic("The kth value exists and should be found");
+    }
+}
+
+test "kth > len short circuits" {
+    const haystack = [0]u8{};
+    const res = k_largest(u8, std.testing.allocator, &haystack, 10, null);
+    try expectEqual(res, null);
 }

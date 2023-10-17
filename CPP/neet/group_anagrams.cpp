e445d209e1743e34a1d6de39adbf54c830f55710
Josh Megnauth <jo.sh@tutanota.com>
2023-10-16 22:47:11 -0400

    Add group anagrams leet code (C++)

--- CPP/neet/group_anagrams.cpp
+++ CPP/neet/group_anagrams.cpp
@@ -1,17 +1,17 @@
 #include "group_anagrams.hpp"
 
 #include <algorithm>
-#include <hash>
-#include <map>
+#include <cassert>
 #include <span>
 #include <string>
 #include <string_view>
+#include <unordered_map>
 #include <vector>
 
-using std::map;
 using std::span;
 using std::string;
 using std::string_view;
+using std::unordered_map;
 using std::vector;
 using std::operator""sv;
 
@@ -25,8 +25,8 @@ string_view strip_whitespace(string_view s) {
   return s;
 }
 
-vector<vector<string_view>> group_anagrams(span<string> words) {
-  map<string, vector<string_view>> groups;
+vector<vector<string_view>> group_anagrams(span<const string> words) {
+  unordered_map<string, vector<string_view>> groups;
 
   for (const auto &word : words) {
     auto stripped = string(strip_whitespace(word));
@@ -35,8 +35,22 @@ vector<vector<string_view>> group_anagrams(span<string> words) {
     std::transform(stripped.begin(), stripped.end(), stripped.begin(),
                    [](unsigned char ch) { return std::tolower(ch); });
 
-    // Sort and hash string
+    // Sort and insert string
     std::sort(stripped.begin(), stripped.end());
-    const auto hash = std::hash<string>{}(stripped);
+    if (groups.contains(stripped)) {
+      groups[stripped].emplace_back(word);
+    } else {
+      vector<string_view> anagrams = {word};
+      const auto [_it, success] = groups.emplace(stripped, anagrams);
+      assert(success);
+    }
   }
+
+  // Move map's values (the grouped anagrams) into a vector
+  vector<vector<string_view>> anagrams;
+  anagrams.reserve(groups.size());
+  std::transform(groups.begin(), groups.end(), std::back_inserter(anagrams),
+                 [](const auto pair) { return pair.second; });
+
+  return anagrams;
 }
--- CPP/neet/group_anagrams.hpp
+++ CPP/neet/group_anagrams.hpp
@@ -1,7 +1,6 @@
 #ifndef JOSH_GROUP_ANAGRAMS
 #define JOSH_GROUP_ANAGRAMS
 
-#include <map>
 #include <span>
 #include <string>
 #include <string_view>
@@ -10,6 +9,6 @@
 // Group ASCII words into anagrams.
 // WARNING: Not Unicode aware
 std::vector<std::vector<std::string_view>>
-group_anagrams(std::span<std::string> words);
+group_anagrams(std::span<const std::string> words);
 
 #endif

0eff2643769b7eb766a0addf5962716da7b6015d
Josh Megnauth <jo.sh@tutanota.com>
2023-10-30 01:43:02 -0400

    Update two sum ordered (Rust)
    - Generic over integers + tests

--- Rust/jobspls/src/neet/two_sum_ordered.rs
+++ Rust/jobspls/src/neet/two_sum_ordered.rs
@@ -1,11 +1,20 @@
-use std::cmp::Ordering;
+use num::{CheckedAdd, Integer};
+use std::{
+    cmp::Ordering,
+    fmt::{Debug, Display},
+};
 
-pub fn two_sum_ordered(haystack: &[i32], target: i32) -> Option<(usize, usize)> {
+pub fn two_sum_ordered<I>(haystack: &[I], target: I) -> Option<(usize, usize)>
+where
+    I: Integer + CheckedAdd + Clone + Copy + Debug + Display,
+{
     let mut left = 0;
     let mut right = haystack.len() - 1;
 
     while left < right {
-        let sum = haystack[left] + haystack[right];
+        let Some(sum) = haystack[left].checked_add(&haystack[right]) else {
+        continue;
+    };
 
         match sum.cmp(&target) {
             Ordering::Less => left += 1,
@@ -15,4 +24,36 @@ pub fn two_sum_ordered(haystack: &[i32], target: i32) -> Option<(usize, usize)> 
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::two_sum_ordered;
+
+    #[test]
+    fn test_two_sum_ii_ex_1() {
+        let haystack = [2, 7, 11, 15];
+        let target = 9;
+
+        let (i, j) = two_sum_ordered(&haystack, target).expect("should find both indices");
+        assert_eq!(target, haystack[i] + haystack[j]);
+    }
+
+    #[test]
+    fn test_two_sum_ii_ex_2() {
+        let haystack = [2, 3, 4];
+        let target = 6;
+
+        let (i, j) = two_sum_ordered(&haystack, target).expect("should find both indices");
+        assert_eq!(target, haystack[i] + haystack[j]);
+    }
+
+    #[test]
+    fn test_two_sum_ii_ex_3() {
+        let haystack = [-1, 0];
+        let target = -1;
+
+        let (i, j) = two_sum_ordered(&haystack, target).expect("should find both indices");
+        assert_eq!(target, haystack[i] + haystack[j]);
+    }
 }

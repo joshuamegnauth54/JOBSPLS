f749ded08e88a981bfee72aac306721c5ca56549
Josh Megnauth <jo.sh@tutanota.com>
2023-12-06 21:18:15 -0500

    Less ugly replace vals right LeetCode (Rust)
    Previous solution allocated twice.

--- Rust/jobspls/src/neet/replace_vals_right.rs
+++ Rust/jobspls/src/neet/replace_vals_right.rs
@@ -1,18 +1,17 @@
 pub fn replace_vals_right_iter(nums: &[i32]) -> Vec<i32> {
-    nums.iter()
+    let mut out: Vec<_> = nums
+        .iter()
         .copied()
-        .skip(1)
         .rev()
-        .scan(nums.last().copied().unwrap_or(-1), |state, x| {
+        .scan(-1, |state, x| {
+            let current = *state;
             *state = x.max(*state);
-            Some(*state)
+            Some(current)
         })
-        // This is admittedly worse than the imperative solution
-        .collect::<Vec<_>>()
-        .into_iter()
-        .rev()
-        .chain([-1])
-        .collect()
+        .collect();
+    // Can't reverse a `Scan`. RIP.
+    out.reverse();
+    out
 }
 
 #[cfg(test)]
@@ -23,6 +22,15 @@ mod tests {
     fn replace_vals_right_ex1() {
         let values = [17, 18, 5, 4, 6, 1];
         let expected = [18, 6, 6, 6, 1, -1];
+        let actual = replace_vals_right_iter(&values);
+
+        assert!(expected.iter().copied().eq(actual.into_iter()));
+    }
+
+    #[test]
+    fn replace_vals_right_ex2() {
+        let values = [400];
+        let expected = [-1];
         let actual = replace_vals_right_iter(&values);
 
         assert!(expected.iter().copied().eq(actual.into_iter()));

54de38197efd52eb83fc1e7a7b27b98e8581650d
Josh Megnauth <jo.sh@tutanota.com>
2024-05-18 22:35:53 -0400

    Smallest pos int unit tests (Rust)

--- Rust/jobspls/src/codility/smallest_pos_int.rs
+++ Rust/jobspls/src/codility/smallest_pos_int.rs
@@ -12,3 +12,29 @@ where
     })
     .find(|val| !hayset.contains(val))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::smallest_pos_int;
+
+    #[test]
+    fn missing_one_pos_is_found() {
+        let actual = smallest_pos_int(&[2, 3, 4]).expect("Should find a value");
+        let expected = 1;
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn missing_one_neg_is_found() {
+        let actual = smallest_pos_int(&[-1, -2]).expect("Should find a value");
+        let expected = 1;
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn missing_one_zero_is_found() {
+        let actual = smallest_pos_int(&[0]).expect("Should find a value");
+        let expected = 1;
+        assert_eq!(expected, actual);
+    }
+}
